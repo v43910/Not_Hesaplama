ef9e20ba5499dd2d69c0af2b7d7e9ea6b6447a16
veysel küçükkaraca <v43910@gmail.com>
2021-11-25 23:45:15 +0300

    Add project files.

---
+++ LabratuvarOdevi211103/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace LabratuvarOdevi211103
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDevamsizlik = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnHesapla);
+            this.groupBox1.Controls.Add(this.txtFinal);
+            this.groupBox1.Controls.Add(this.txtSoyadi);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtDevamsizlik);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtVize);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtAdi);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(603, 81);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Öğrencinin Bilgileri";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(417, 45);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(174, 23);
+            this.btnHesapla.TabIndex = 6;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(303, 45);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(79, 20);
+            this.txtFinal.TabIndex = 4;
+            this.txtFinal.Text = "0";
+            this.txtFinal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtFinal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sadeceRakam);
+            this.txtFinal.Leave += new System.EventHandler(this.txtFinal_Leave);
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(74, 45);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(150, 20);
+            this.txtSoyadi.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Final Notu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyadı";
+            // 
+            // txtDevamsizlik
+            // 
+            this.txtDevamsizlik.Location = new System.Drawing.Point(512, 19);
+            this.txtDevamsizlik.Name = "txtDevamsizlik";
+            this.txtDevamsizlik.Size = new System.Drawing.Size(79, 20);
+            this.txtDevamsizlik.TabIndex = 5;
+            this.txtDevamsizlik.Text = "0";
+            this.txtDevamsizlik.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtDevamsizlik.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sadeceRakam);
+            this.txtDevamsizlik.Leave += new System.EventHandler(this.txtDevamsizlik_Leave);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(414, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Devamsızlık Günü";
+            // 
+            // txtVize
+            // 
+            this.txtVize.Location = new System.Drawing.Point(303, 19);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(79, 20);
+            this.txtVize.TabIndex = 3;
+            this.txtVize.Text = "0";
+            this.txtVize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtVize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sadeceRakam);
+            this.txtVize.Leave += new System.EventHandler(this.txtVize_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Vize Notu";
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(74, 19);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(150, 20);
+            this.txtAdi.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 99);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(606, 288);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Öğrenciler";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(14, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(577, 251);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 404);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Not Hesaplama";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDevamsizlik;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ LabratuvarOdevi211103/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabratuvarOdevi211103
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        string hesaplamaSonucu = "";
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int basariNotu = 0;
+            bool gectiMi = false;
+
+
+            if (txtAdi.Text!=string.Empty && txtSoyadi.Text!=string.Empty)
+            {
+                basariNotu = (int)(Convert.ToInt16(txtVize.Text) * 0.40) + (int)(Convert.ToInt16(txtFinal.Text) * 0.60);
+                hesaplamaSonucu = txtAdi.Text + " " + txtSoyadi.Text+"    "+basariNotu+"    ";
+                
+                switch (katsayiHesapla(basariNotu))
+                {
+                     case 4.0f:
+                        hesaplamaSonucu += "  AA  ";
+                        gectiMi = true;
+                        break;
+                    case 3.5f:
+                        hesaplamaSonucu += "  BA  ";
+                        gectiMi = true;
+                        break;
+                    case 3.0f:
+                        hesaplamaSonucu += "  BB  ";
+                        gectiMi = true;
+                        break;
+                    case 2.5f:
+                        hesaplamaSonucu += "  CB  ";
+                        gectiMi = true;
+                        break;
+                    case 2.0f:
+                        hesaplamaSonucu += "  CC  ";
+                        gectiMi = true;
+                        break;
+                    case 1.5f:
+                        hesaplamaSonucu += "  DC  ";
+                        break;
+                    case 1.0f:
+                        hesaplamaSonucu += "  DD  ";
+                        break;
+                    default:
+                        hesaplamaSonucu += "  FD  ";
+                        break;
+                }
+
+                if (gectiMi && Convert.ToInt16(txtDevamsizlik.Text) > 10)
+                {
+                    hesaplamaSonucu += "   Kaldı (Devamsızlık)";
+                }
+                else if (!gectiMi)
+                {
+                    hesaplamaSonucu += "   Kaldı (Ortalama)";
+                }
+                else
+                {
+                    hesaplamaSonucu += "   Gecti";
+                }
+
+                listBox1.Items.Add(hesaplamaSonucu);
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Öğrencinin Adını Soyadını giriniz.");
+            }
+        }
+
+        private float katsayiHesapla(int hesaplananNot) {
+            float katsayi = 0;
+
+            if (hesaplananNot >= 90)
+            {
+                katsayi = 4.0f;
+            }
+            else if (hesaplananNot > 84 && hesaplananNot < 90)
+            {
+                katsayi = 3.5f;
+            }
+            else if (hesaplananNot > 79 && hesaplananNot < 85)
+            {
+                katsayi = 3;
+            }
+            else if (hesaplananNot > 74 && hesaplananNot < 80)
+            {
+                katsayi = 2.5f;
+            }
+            else if (hesaplananNot > 64 && hesaplananNot < 75)
+            {
+                katsayi = 2;
+            }
+            else if (hesaplananNot > 59 && hesaplananNot < 65)
+            {
+                katsayi = 1.5f;
+            }
+            else
+            {
+                katsayi = 1;
+            }
+            return katsayi;
+        }
+
+        //sadece rakamsal girişi denetler.(txtVize, txtFinal, txtDevamsızlık) 
+        private void sadeceRakam(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+        //boş bırakılırsa 0 -sıfır- yazar
+        private void txtVize_Leave(object sender, EventArgs e)
+        {
+            if (txtVize.Text==string.Empty)
+            {
+                txtVize.Text = "0";
+            }
+        }
+
+        private void txtFinal_Leave(object sender, EventArgs e)
+        {
+            if (txtFinal.Text == string.Empty)
+                txtFinal.Text = "0";
+        }
+
+        private void txtDevamsizlik_Leave(object sender, EventArgs e)
+        {
+            if (txtDevamsizlik.Text == string.Empty)
+                txtDevamsizlik.Text = "0";
+        }
+    }
+}
